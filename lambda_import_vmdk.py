8b3c5e8dede81bdc9f9ca825d13ab26857ac261b
virtlima <lima.aaron@gmail.com>
2016-09-22 12:24:54 -0400

    Add files via upload

---
+++ lambda_import_vmdk.py
@@ -0,0 +1,27 @@
+import boto3
+
+def lambda_handler(event, context):
+	ec2_client = boto3.client('ec2')
+	s3_object = event[u'Records'][0][u's3'][u'object'][u'key']
+	s3_bucket = event[u'Records'][0][u's3'][u'bucket'][u'name']
+
+	import_vmdk = ec2_client.import_image(
+		DryRun=True,
+		Description='Lambda_VMIE',
+		DiskContainers=[
+			{
+				'Description':'Import_VMDK',
+				'Format':'vmdk',
+				'UserBucket':
+					{
+						'S3Bucket': s3_bucket,
+						'S3Key': s3_object 
+					},
+				},
+			],
+		LicenseType='BYOL'
+	)
+	
+	import_vmdk
+
+	return;
